8206ba8158d523a9d308cdf876655b6e260e1700
Maksym Yurchenko <mvy@norse.digital>
2022-02-08 13:10:36 +0200

    Heroku

--- src/routes/decode.ts
+++ src/routes/decode.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import jwt_decode from "jwt-decode";
 
 function get(req: Request, res: Response, next: NextFunction) {
+  console.log(1);
   try {
     const token = req.body.token;
     const decoded = jwt_decode(token);
